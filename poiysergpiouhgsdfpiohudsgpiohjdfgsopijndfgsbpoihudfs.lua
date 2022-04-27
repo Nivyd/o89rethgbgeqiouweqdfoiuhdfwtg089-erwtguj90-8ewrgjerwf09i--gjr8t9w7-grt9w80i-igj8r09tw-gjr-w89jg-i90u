e9b70d39e74da38385b269a527730ad5a3faea03
Nivyd <86750924+Nivyd@users.noreply.github.com>
2022-04-27 17:02:30 -0300

    Update poiysergpiouhgsdfpiohudsgpiohjdfgsopijndfgsbpoihudfs.lua

--- poiysergpiouhgsdfpiohudsgpiohjdfgsopijndfgsbpoihudfs.lua
+++ poiysergpiouhgsdfpiohudsgpiohjdfgsopijndfgsbpoihudfs.lua
@@ -1,12 +1,11 @@
 IsPlaying = true -- (true = come�ar)/(false = parar) -- ID (obs: mesmo id do V4)
 local Name = "" -- Name (nome do audio na workspace)
-local Volume = 100 -- Volume (100 pra dar ficar alto, padr�o � 1, m�ximo � 100 e nao � por regras do script n, � do pr�prio roblox que permite.)
 local Pitch = 1 -- Pitch (pitch = velocidade, o padr�o � 1)
 
 for i,v in pairs(game:GetService("Workspace"):GetDescendants()) do
     if v:IsA("RemoteEvent") and v.Name == "AC6_FE_Sounds" then
         if IsPlaying == true then
-            v:FireServer("newSound", Name, workspace, getgenv().Id, Pitch, Volume, true)
+            v:FireServer("newSound", Name, workspace, getgenv().Id, Pitch, getgenv().Volume, true)
             v:FireServer("playSound", Name)
         end 
 
